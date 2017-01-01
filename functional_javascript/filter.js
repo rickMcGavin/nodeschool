8b9f0d8f3794dfe3d3338299f184766221e82de8
rebelBeard <rfm144@gmail.com>
2016-12-31 20:54:39 -0500

    solution for Basic: Filter
    - filter.js

--- functional_javascript/filter.js
+++ functional_javascript/filter.js
@@ -1,6 +1,15 @@
 function getShortMessages(messages) {
   // SOLUTION GOES HERE
-  
+  return messages.filter(function(msg) {
+    return msg.message.length < 50;
+  }).map(function(msg) {
+    return msg.message;
+  });
 }
 
 module.exports = getShortMessages;
+
+/*
+note: I learned you could chain methods in this fashion from
+https://egghead.io/lessons/javascript-chaining-the-array-map-and-filter-methods
+*/
